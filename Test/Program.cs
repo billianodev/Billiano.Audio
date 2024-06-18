35be257162d09b0fe507e24bcfc33715f91728fe
Darwin Billiano <darwinbilliano@outlook.com>
2024-06-18 19:34:56 +0700

    Updated test project

--- Test/Program.cs
+++ Test/Program.cs
@@ -1,13 +1,27 @@
-﻿using Billiano.Audio;
-using Billiano.Audio.FireForget;
-using Billiano.Audio.PortAudio;
-
-/*
+﻿/*
  * This project is a sample code to Billiano.Audio packages
  * Run this sample only in DEBUG mode!
  */
 
+/*
+ * (Un)comment line below to switch between fire forget mode and direct output
+ */
+#define USE_FIRE_FORGET
+
+using Billiano.Audio;
+using Billiano.Audio.PortAudio;
+using NAudio.Wave;
+
+#if USE_FIRE_FORGET
+using Billiano.Audio.FireForget;
+#endif
+
 var codecFactory = CodecFactory.CreateDefault();
+
+#if USE_FIRE_FORGET
+using var player = new PortAudioOut();
+using var fireForgetPlayer = new FireForgetPlayer(player, new WaveFormat(44100, 1));
+#endif
 
 Console.ReadLine();
 Play("test.mp3");
@@ -18,11 +32,15 @@ return;
 
 void Play(string path)
 {
-    using var reader = codecFactory.GetCodec(path);
-    var source = reader.ToFireForgetSource();
+    var reader = codecFactory.GetCodec(path);
+    var source = reader.ToWaveCache();
     
+#if USE_FIRE_FORGET
+    fireForgetPlayer.Play(source);
+#else
     var player = new PortAudioOut();
     player.Init(source.ToWaveProvider());
     player.Play();
     player.PlaybackStopped += (sender, eventArgs) => player.Dispose();
+#endif
 }
